ff677f100a87ddf3b9eb3fb04a4d0fc373acb28a
Vinicius Romani <vinicius.romani@moobie.com.br>
2018-08-16 00:54:20 -0300

    Initial Commit

---
+++ IndieCalendar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IndieCalendar
+//
+//  Created by Vinicius Romani on 16/08/18.
+//  Copyright © 2018 Vinicius Romani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
